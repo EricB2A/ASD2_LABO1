3063cba0c76c37f09f8621a9d59a1a5b688e0ff5
ilias <ilias.goujgali@cpnv.ch>
2019-09-21 14:06:58 +0200

    implement graphFromImage class

--- ex 1/GraphFromImage.cpp
+++ ex 1/GraphFromImage.cpp
@@ -1,29 +1,66 @@
 #include "GraphFromImage.h"
 
+using namespace std;
 GraphFromImage::GraphFromImage(const bitmap_image& i) : image(i) {
-    /* A IMPLEMENTER */
 }
 
 // throws std::out_of_range
 GraphFromImage::Iterable GraphFromImage::adjacent(int v) const {
-    /* A IMPLEMENTER */
+    Iterable neighbours;
+
+    // add pixel from right side only if the pixel is not in the first column
+    if ((unsigned) x(v) > 0 && pixelSameColor(v, v - 1)) {
+        neighbours.push_back(v - 1);
+    }
+
+    // add pixel from left side only if the pixel is not in the last column
+    if ((unsigned) x(v) < (image.width() - 1) && pixelSameColor(v, v + 1)) {
+        neighbours.push_back(v + 1);
+    }
+
+    // add pixel from above only if the pixel is  not in the first row
+    if (y(v) > 0 && pixelSameColor(v, v - image.width())) {
+        neighbours.push_back(v - image.width());
+    }
+
+    // add pixel from below only if the pixel is not in the last row
+    if ((unsigned) y(v) < (image.height() - 1)
+        && pixelSameColor(v, v + image.width())) {
+        neighbours.push_back(v + image.width());
+    }
+    return neighbours;
 }
 
 // throws std::out_of_range
 int GraphFromImage::idx(int x, int y) const {
-    /* A IMPLEMENTER */
+    if ((unsigned) x >= image.width() || (unsigned) y >= image.height())
+        throw new std::out_of_range("Sac à vin");
+    return y * image.width() + x;
 }
 
 // throws std::out_of_range
 int GraphFromImage::x(int idx) const {
-    /* A IMPLEMENTER */
+    if ((unsigned) idx > (image.width() * image.height()) - 1)
+        throw new std::out_of_range("Sac à vin");
+    return (unsigned) idx % image.width();
 }
 
 // throws std::out_of_range
 int GraphFromImage::y(int idx) const {
-    /* A IMPLEMENTER */
+    if ((unsigned) idx > (image.height() * image.width()) - 1)
+        throw new std::out_of_range("Sac à vin");
+    return (unsigned) idx / image.width();
 }
 
 int GraphFromImage::V() const {
-    /* A IMPLEMENTER */
+    return (int) (image.width() * image.height());
+}
+bool GraphFromImage::pixelSameColor(const int lPixel, const int rPixel) const {
+    unsigned char lR, lG, lB;
+    unsigned char rR, rG, rB;
+
+    image.get_pixel(x(lPixel), y(lPixel), lR, lG, lB);
+    image.get_pixel(x(rPixel), y(rPixel), rR, rG, rB);
+
+    return (lR == rR && lG == rG && lB == rB);
 }
