a031eb2133af70be7b664d1ef878cf510153aaac
Eric Broutba <eric.bousbaa@hotmail.com>
2019-09-28 15:02:43 +0200

    graph implémentation

--- ex 2/SymbolGraph.h
+++ ex 2/SymbolGraph.h
@@ -15,7 +15,7 @@
 #include <sstream>
 #include <algorithm>
 #include <vector>
-
+#include <map>
 #include "Util.h"
 
 template<typename GraphType>
@@ -24,7 +24,8 @@ class SymbolGraph
     typedef GraphType Graph; 
 private:
     Graph* g;
-    // A IMPLEMENTER: vos structures privées ici.
+    std::vector<std::string> symbols;
+    std::map<std::string, int> indices;
     
 public:
     
@@ -35,44 +36,77 @@ public:
     
     //creation du SymbolGraph a partir du fichier movies.txt
     SymbolGraph(const std::string& filename) {
-        /* A IMPLEMENTER */
-        // Indication: nous autorisons une double lecture du fichier.
-
-        // exemple de lecture du fichier, ligne par ligne puis element par element (separe par des /)
         std::string line;
+        unsigned cnt = 0;
 
         std::ifstream s(filename);
         while (std::getline(s, line))
         {
             auto names = split(line,'/');
-            
+            std::string movie_title = names.front(); 
+            //std::cout << movie_title;
+            symbols.push_back(movie_title);
+            indices[movie_title] = cnt;
             for( auto name : names ) {
-                std::cout << name << " ";
+                //std::cout << name << " ";
+                symbols.push_back(name);
+                indices[symbols[cnt]] = cnt;
+                cnt++;
+                
             }
             
-            std::cout << std::endl;
+            //std::cout << std::endl;
+
         }
-        s.close();
+
+        s.clear();
+      s.seekg(0, std::ios::beg);
+
+      // init the graph with the number of symbols/indices/"nodes"
+      g = new Graph(symbols.size());
+
+      // browse the file again and add the edges between the movie and the actors
+      while (std::getline(s, line)) {
+          std::vector<std::string> names = split(line, '/');
+          int movieNum = indices[names[0]];
+
+          for (unsigned i = 1; i < names.size(); ++i) {
+              g->addEdge(movieNum, indices[names[i]]);
+          }
+
+      }
+      s.close();
+        
     }
     
     //verifie la presence d'un symbole
     bool contains(const std::string& name) const {
         /* A IMPLEMENTER */
+        return std::find(symbols.begin(), symbols.end(), name) != symbols.end();
     }
     
     //index du sommet correspondant au symbole
     int index(const std::string& name) const {
          /* A IMPLEMENTER */
+        return indices.at(name);
     }
     
     //symbole correspondant au sommet
     std::string symbol(int idx) const {
         /* A IMPLEMENTER */
+        return symbols.at(idx);
     }
 
     //symboles adjacents a un symbole
     std::vector<std::string> adjacent(const std::string& name) const {
         /* A IMPLEMENTER */
+        std::vector<std::string> adjacents;
+
+        for (int i : g->adjacent(index(name))) {
+            adjacents.push_back(this->symbol(i));
+        }
+
+        return adjacents;
     }
     
     const Graph& G() const {
